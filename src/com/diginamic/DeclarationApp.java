fa8840cd30e748494bd682d4b3d3ae2c0f2b5af4
RichardBONNAMY <rbonnamy@diginamic.fr>
2021-01-22 15:53:40 +0100

    v1

---
+++ src/com/diginamic/DeclarationApp.java
@@ -0,0 +1,30 @@
+package com.diginamic;
+
+public class DeclarationApp {
+	public static void main(String[] args) {
+
+		byte a = 5;
+		short c = 12;
+		int v = 1289;
+		
+		v = v+1;
+		v = v/2;		
+		
+		long varlong = 1254;
+		
+		float f = 1.256f;
+		double d= 125.125;
+		
+		char cc = '\u1589';
+		String s = "Bonjour, le résultat du calcul est :\n 125";
+		
+		System.out.println(a);
+		System.out.println(c);
+		System.out.println(v);
+		System.out.println(varlong);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(cc);
+		System.out.println(s);
+	}
+}
